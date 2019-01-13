1d699d773b830751722985d8f9e228c2d6209213
barakgahtan <barakgahtan@gmail.com>
2019-01-13 11:21:36 +0200

    experminets

---
+++ Additional_tests.py
@@ -0,0 +1,5 @@
+import sklearn
+
+
+
+DecisionTreeClassifier(criterion="entropy")
---
+++ KNN_test.py
@@ -0,0 +1,14 @@
+import csv
+from classifier import knn_factory, evaluate
+
+results = []
+
+for k in [1, 3, 5, 7, 13]:
+    knn_k = knn_factory(k)
+    avg_accuracy, avg_error = evaluate(knn_k, 2)
+    results.append([k, avg_accuracy, avg_error])
+
+with open("experiments/experiments6.csv", "w") as csv_file:
+    writer = csv.writer(csv_file, delimiter=',', lineterminator='\n')
+    for row in results:
+        writer.writerow(row)
