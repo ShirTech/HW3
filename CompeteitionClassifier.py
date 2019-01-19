0f50e9d42924bb390151a2b384efa44092951e25
Shir <shirco@campus.technion.ac.il>
2019-01-19 17:17:21 +0200

    for barak

--- CompeteitionClassifier.py
+++ CompeteitionClassifier.py
@@ -1,27 +1,15 @@
-import pickle
-
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 
-from hw3_utils import load_data
+from hw3_utils import load_data, write_prediction
 from our_hw3_utils import feature_selection
 
-factor = {
-    "ID3": 1,
-    "3-Nearest Neighbors": 4,
-    "Neural Net": 4,
-    "AdaBoost": 3,
-    "RandomForest": 3,
-}
+names = ["ID3", "MinLeaf", "limited_ID3", "1-Nearest Neighbors", "1-Nearest Neighbors", "1-Nearest Neighbors",
+         "2-Nearest Neighbors", "2-Nearest Neighbors", "3-Nearest Neighbors", "RandomForest"]
 
-# names = ["ID3", "3-Nearest Neighbors", "Neural Net", "AdaBoost", "RandomForest"]
-names = ["ID3", "MinLeaf", "vd", "MinLeafedf", "MinLeaf2", "1-Nearest Neighbors", "2-Nearest Neighbors",
-         "3-Nearest Neighbors", "VD", "RandomForest"]
-
-# DecisionTreeClassifier(criterion="entropy", max_depth= 2, min_samples_leaf= 20)
 classifiers = [
     DecisionTreeClassifier(criterion="entropy"),
     DecisionTreeClassifier(min_samples_leaf=3),
@@ -35,16 +23,9 @@ classifiers = [
     RandomForestClassifier()]
 
 
-# X_train = data
-# y_train = getLabels(train_group)
-# X_test = test_group_features
-# y_test = test_group_labels
-
-
-def convertFeaturesToResFet(X_train, y_train, features):
+def convertFeaturesToResultFeatures(X_train, y_train, features):
     clf_results = []
     for name, clf in zip(names, classifiers):
-        # clf_factor = factor[name]
         clf.fit(X_train, y_train)
         train_predict = clf.predict(features)
 
@@ -59,73 +40,9 @@ def convertFeaturesToResFet(X_train, y_train, features):
     return clf_results
 
 
-def newClassifier(X_train, y_train, X_test, y_test):
-    clf_results_train = convertFeaturesToResFet(X_train, y_train, X_train)
-    clf_results_test = convertFeaturesToResFet(X_train, y_train, X_test)
-
-    new_train = []
-    for features, extra in zip(X_train, clf_results_train):
-        extra = np.array(extra)
-        new_train.append(np.append(features, extra))
-
-    new_test = []
-    for features, extra in zip(X_test, clf_results_test):
-        extra = np.array(extra)
-        new_test.append(np.append(features, extra))
-
-    # clf_results_train = X_train.extend(clf_results_train)
-    # clf_results_test = X_test.extend(clf_results_test)
-
-    # at this point clf_factor contains 499 samples of features: each feature is the result of a certain classifier and now we'll train a new classifer using this features
-    final_clf = classifiers[2]
-    final_clf.fit(new_train, y_train)
-
-    score_train = final_clf.score(new_train, y_train)
-    score_test = final_clf.score(new_test, y_test)
-    # print('Accuracy of ' + "MAXX" + ' on training set: ' + str(score_train))
-    # print('Accuracy of ' + "MAXX" + ' on test set: ' + str(score_test))
-    return score_train, score_test
-
-#
-# newClassifier()
-# test_avg = 0
-# train_avg = 0
-# for i in range(5):
-#     print(i)
-#     score_train, score_test = newClassifier()
-#     train_avg += score_train
-#     test_avg += score_test
-#     print('current accuracy of ' + "MAXX" + ' on test set: ' + str(score_test))
-#
-# print('Avg accuracy of ' + "MAXX" + ' on training set: ' + str(train_avg/50))
-# print('Avg accuracy of ' + "MAXX" + ' on test set: ' + str(test_avg/50))
-
-# x_train_subset and x_test_subset are updated with relevant features only
-
-# newClassifier(X_train_subset, y_train, X_test_subset, y_test)
-#
-# with open("updated_features.data", 'rb') as f:
-#     X_train_subset, X_test_subset = pickle.load(f)
-#
-# test_avg = 0
-# train_avg = 0
-# iterations = 50
-# for i in range(iterations):
-#     print(i)
-#     score_train, score_test = newClassifier(X_train_subset, y_train, X_test_subset, y_test)
-#     train_avg += score_train
-#     test_avg += score_test
-#     print('current accuracy of ' + "MAXX" + ' on test set: ' + str(score_test))
-#
-# print('Avg accuracy of ' + "MAXX" + ' on training set: ' + str(train_avg / iterations))
-# print('Avg accuracy of ' + "MAXX" + ' on test set: ' + str(test_avg / iterations))
-#
-
 def test():
     train_features, train_labels, test_features = load_data()
-    X_train_subset = feature_selection(train_features, train_labels)
-    # with open("updated_features_2.data", 'rb') as f:
-    #     X_train_subset = pickle.load(f)
+    X_train_subset, X_test_subset = feature_selection(train_features, train_labels)
 
     total_avg = 0
     for k in [2, 4, 6, 8, 10]:
@@ -135,7 +52,7 @@ def test():
         for i in range(k):
             X_train, X_test, y_train, y_test = train_test_split(X_train_subset, train_labels, test_size=test_size,
                                                                 random_state=i)
-            clf = MAXX(X_train, y_train)
+            clf = CompetitionClassifier(X_train, y_train)
             total_score_per_k_train += clf.test(X_train, y_train)
             total_score_per_k_test += clf.test(X_test, y_test)
         print('K value is: ' + str(k))
@@ -143,26 +60,42 @@ def test():
         total_avg += total_score_per_k_test / k
     print('Total avg for all k is: ' + str(total_avg / 5))
 
+    pred = clf.classify(X_test_subset)
+    write_prediction(pred)
 
-class MAXX():
+
+class CompetitionClassifier():
     def __init__(self, data, labels):
         self.X = data
         self.y = labels
 
-    def classify(self, features):
-        clf_results_features = convertFeaturesToResFet(self.X, self.y, features)
-        new_features = []
-        for features, extra in zip(features, clf_results_features):
-            extra = np.array(extra)
-            new_features.append(np.append(features, extra))
+    def classify(self, X_test):
+        X_train = self.X
+        y_train = self.y
+        clf_results_train = convertFeaturesToResultFeatures(X_train, y_train, X_train)
+        clf_results_test = convertFeaturesToResultFeatures(X_train, y_train, X_test)
 
-        return self.clf.predict(new_features)
+        new_train = []
+        for features, extra in zip(X_train, clf_results_train):
+            extra = np.array(extra)
+            new_train.append(np.append(features, extra))
+
+        new_test = []
+        for features, extra in zip(X_test, clf_results_test):
+            extra = np.array(extra)
+            new_test.append(np.append(features, extra))
+
+        final_clf = KNeighborsClassifier(1)
+        final_clf.fit(new_train, y_train)
+
+        prediction = final_clf.predict(X_test)
+        return prediction
 
     def test(self, X_test, y_test):
         X_train = self.X
         y_train = self.y
-        clf_results_train = convertFeaturesToResFet(X_train, y_train, X_train)
-        clf_results_test = convertFeaturesToResFet(X_train, y_train, X_test)
+        clf_results_train = convertFeaturesToResultFeatures(X_train, y_train, X_train)
+        clf_results_test = convertFeaturesToResultFeatures(X_train, y_train, X_test)
 
         new_train = []
         for features, extra in zip(X_train, clf_results_train):
--- our_hw3_utils.py
+++ our_hw3_utils.py
@@ -1,6 +1,4 @@
 import copy
-import copy
-import pickle
 
 from sklearn.model_selection import cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
@@ -8,123 +6,54 @@ from sklearn.neighbors import KNeighborsClassifier
 
 def sfs(x, y, required_features_num, clf, score):
     number_of_features = len(x[0])
-    features_chosen = []
-    # x subset will contain required features only
+    number_of_samples = len(y)
+    chosen_features = []
+
+    # TODO: x subset will contain required features only
     x_subset = [[] for i in range(len(x))]
 
-    while len(features_chosen) < required_features_num:
+    for i in range(required_features_num):
         best_score = -1
         best_index = -1
 
         for feature_index in range(number_of_features):
-            if feature_index in features_chosen:
-                continue
 
-            # create x_subset_temp containing values for all chosen features and the new feature the we are considering
-            x_subset_temp = copy.deepcopy(x_subset)
-            for feature_list in range(len(x)):
-                x_subset_temp[feature_list].append(x[feature_list][feature_index])
+            if feature_index not in chosen_features:
 
-            current_score = score(clf, x_subset_temp, y)
-            if current_score > best_score:
-                best_score = current_score
-                best_index = feature_index
+                x_subset_temp = copy.deepcopy(x_subset)
+                for feature_list in range(number_of_samples):
+                    x_subset_temp[feature_list].append(x[feature_list][feature_index])
 
-                # update features_chosen to contain the new feature's index and x_subset to contain its values
-        features_chosen.append(best_index)
+                current_score = score(clf, x_subset_temp, y)
+                if current_score > best_score:
+                    best_score = current_score
+                    best_index = feature_index
+
+        # TODO: update chosen_features to contain the new feature's index and x_subset to contain its values
+        chosen_features.append(best_index)
         for feature_list in range(len(x)):
             x_subset[feature_list].append(x[feature_list][best_index])
 
-    return features_chosen
+    return chosen_features
 
 
-def sbs(x, y, k, clf, score):
-    """
-    :param x: feature set to be trained using clf. list of lists.
-    :param y: labels corresponding to x. list.
-    :param k: number of features to select. int
-    :param clf: classifier to be trained on the feature subset.
-    :param score: utility function for the algorithm, that receives clf, feature subset and labeles, returns a score.
-    :return: list of chosen feature indexes
-    """
-
-    import copy
-    number_of_features = len(x[0])
-
-    # indecis of chosen features
-    features_for_removal = []
-
-    # x containg only values of chosen features
-    x_subset = copy.deepcopy(x)
-
-    while len(features_for_removal) < k:
-        min_score = 2
-        best_index = -1
-
-        for feature_index in range(number_of_features):
-            # feature already removed, don't consider it anymore
-            if feature_index in features_for_removal:
-                continue
-
-            # create x_subset_temp containing values for all chosen features and the new feature the we are considering
-            x_subset_temp = copy.deepcopy(x_subset)
-            for object_index in range(len(x)):
-                x_subset_temp[object_index][feature_index] = 0
-
-            temp_score = score(clf, x_subset_temp, y)
-            if temp_score < min_score:
-                min_score = temp_score
-                best_index = feature_index
-
-        # update features_for removal to contain the new feature's index and x_subset to contain its values
-        features_for_removal.append(best_index)
-        for object_index in range(len(x)):
-            x_subset[object_index][best_index] = 0
-
-    return features_for_removal
-
-
-def utilityFunction(clf, x, y):
-    # estimate features subset using 4 cross validation
-    # to be used as score function in sfs algorithm
+def evaluate_features(clf, x, y):
     scores = cross_val_score(clf, x, y, cv=4)
     return scores.mean()
 
-def feature_selection(X, y):
-    neigh = KNeighborsClassifier(n_neighbors=3)
-    neigh.fit(X, y)
 
-    features_chosen = sfs(X, y, 10, neigh, utilityFunction)
-    # create x_train_subset and x_test_subset containing only values of chosen features
+def feature_selection(X, y, X_test):
+    KNN = KNeighborsClassifier(n_neighbors=3)
+    KNN.fit(X, y)
+
+    features_chosen = sfs(X, y, 10, KNN, evaluate_features)
+
     X_train_subset = [[] for i in range(len(X))]
-    # X_test_subset = [[] for i in range(len(X_test))]
+    X_test_subset = [[] for i in range(len(X_test))]
     for feature_index in features_chosen:
-        # for object_index in range(len(X_test)):
-        #     X_test_subset[object_index].append(X_test[object_index][feature_index])
+        for object_index in range(len(X_test)):
+            X_test_subset[object_index].append(X_test[object_index][feature_index])
         for object_index in range(len(X)):
             X_train_subset[object_index].append(X[object_index][feature_index])
 
-    path = "updated_features_2.data"
-    with open(path, 'wb') as f:
-        pickle.dump(X_train_subset, f, protocol=pickle.HIGHEST_PROTOCOL)
-
-    return X_train_subset
-
-#
-# features_for_removal = sbs(X_train, y_train, 4, neigh, utilityFunction)
-#
-# # create x_train_subset and x_test_subset containing only values of chosen features
-# x_train_subset = copy.deepcopy(X_train)
-# x_test_subset = copy.deepcopy(X_test)
-# for feature_index in features_for_removal:
-#     for object_index in range(len(X_test)):
-#         x_test_subset[object_index][feature_index] = 0
-#         # x_test_subset[object_index] = np.delete(x_test_subset[object_index], feature_index)
-#     for object_index in range(len(X_train)):
-#         x_train_subset[object_index][feature_index] = 0
-#         # x_train_subset[object_index] = np.delete(x_train_subset[object_index], feature_index)
-#
-#
-# neigh.fit(x_train_subset, y_train)
-# score2 = neigh.score(x_test_subset, y_test)
-# print(score2)
+    return X_train_subset, X_test_subset
